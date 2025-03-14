052bd0d52343414adae5d040d385284f567e5b61
Mohammed <hyperlink.hlpc@gmail.com>
2025-03-14 04:37:06 +0300

    update files

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -3,40 +3,14 @@ import { CartModel } from "./../models/cart.model.js";
 import ApiError from "./../lib/ApiError.js";
 import Order from "./../models/order.model.js";
 import { getAllItems, getOneItem } from "./general.controller.js";
-import {UserService} from "../service/user.service.js";
+import { UserService } from "../service/user.service.js";
 import axios from "axios";
-import {OrderService} from "../service/order.service.js";
-import {ZiinaPaymentStatus as ZinaPaymentStatus, ZiinaPaymentStatus} from "../service/payments/ziina/ziina.types.js";
-import {ZinnaService} from "../service/payments/ziina/zinna.service.js";
-
-
-<<<<<<< HEAD
-export const createZiinaPayment = async (amount, currencyCode, referenceId) => {
-  try {
-    const response = await axios.post(
-      `${process.env.ZIINA_API_URL}/payment_intent`,
-      {
-        amount: amount, // Amount in the smallest currency unit (e.g., cents)
-        currency_code: currencyCode, // Currency code (e.g., AED)
-          // customer_email: userEmail, // Customer's email
-        reference_id: referenceId, // Unique reference ID for the transaction
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.ZIINA_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    console.log(response.data)
-    return response.data;
-  } catch (error) {
-    console.log(error)
-    throw new Error("Failed to create Ziina payment");
-  }
-};
-=======
->>>>>>> 4d095729625eebbe768adb66c39e6a9dcd350368
+import { OrderService } from "../service/order.service.js";
+import {
+  ZiinaPaymentStatus as ZinaPaymentStatus,
+  ZiinaPaymentStatus,
+} from "../service/payments/ziina/ziina.types.js";
+import { ZinnaService } from "../service/payments/ziina/zinna.service.js";
 
 export const checkOutSession = asyncHandler(async (req, res, next) => {
   const taxPrice = 0;
@@ -52,24 +26,19 @@ export const checkOutSession = asyncHandler(async (req, res, next) => {
     ? cart.totalPriceAfterDiscount
     : cart.totalPrice;
   const totalOrderPrice = cartPrice + taxPrice + shippingPrice;
-  const user = await UserService.getUserById(req.user.id)
+  const user = await UserService.getUserById(req.user.id);
   try {
     const payment = await ZinnaService.createZinaPayment(
       totalOrderPrice * 100,
       "AED",
-<<<<<<< HEAD
-        // "alslamat407@gmail.com",
-      req.user._id
-=======
-        cart._id
->>>>>>> 4d095729625eebbe768adb66c39e6a9dcd350368
+      cart._id
     );
     console.log(payment);
     const order = await Order.create({
       user: req.user._id,
       cartItems: cart.cartItems,
       taxPrice,
-      shippingData : 0,
+      shippingData: 0,
       totalOrderPrice,
       paymentMethod: "card",
       isPaid: false,
@@ -91,28 +60,27 @@ export const checkOutSession = asyncHandler(async (req, res, next) => {
 });
 
 export const filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
-    if(req.user.roles === "user") req.body.filterObj = {user: req.user._id}
-    next()
-})
+  if (req.user.roles === "user") req.body.filterObj = { user: req.user._id };
+  next();
+});
 
 export const getPaymentStatus = asyncHandler(async (req, res, next) => {
   try {
     const payment = await axios.get(
-        `${process.env.ZIINA_API_URL}/payment_intent/${req.params.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.ZIINA_API_KEY}`,
-            "Content-Type": "application/json",
-          },
-        }
-    )
-    res.json({payment: payment.data});
-  }catch (error) {
+      `${process.env.ZIINA_API_URL}/payment_intent/${req.params.id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.ZIINA_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    res.json({ payment: payment.data });
+  } catch (error) {
     console.log(error.message);
-    next(error)
+    next(error);
   }
-
-})
+});
 
 export const getAllOrders = getAllItems(Order);
 
@@ -121,17 +89,23 @@ export const getMyOrders = asyncHandler(async (req, res, next) => {
   const order = await OrderService.getMyOrders(user._id);
   res.status(200).json({
     data: order,
-  })
-})
+  });
+});
 
 export const getOrder = asyncHandler(async (req, res, next) => {
-  const order = await OrderService.getOrderById(req.params.id)
-  if(![ZiinaPaymentStatus.canceled, ZiinaPaymentStatus.canceled, ZiinaPaymentStatus.failed].includes(order.status)){
-    const payment = await ZinnaService.getPayment(order.paymentId)
+  const order = await OrderService.getOrderById(req.params.id);
+  if (
+    ![
+      ZiinaPaymentStatus.canceled,
+      ZiinaPaymentStatus.canceled,
+      ZiinaPaymentStatus.failed,
+    ].includes(order.status)
+  ) {
+    const payment = await ZinnaService.getPayment(order.paymentId);
     order.status = payment.status;
-    await order.save()
+    await order.save();
   }
   res.json({
-    data: order
-  })
-})
+    data: order,
+  });
+});
--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -22,10 +22,6 @@ Router.get("/checkout-payments/:cartId", protect,allowedTo("user"), checkOutSess
 Router.get("/check-payment-status/:id", getPaymentStatus)
 Router.get("/:id", protect, getOrder)
 
-<<<<<<< HEAD
-Router.route("/checkout-payment/:cartId").post(protect,allowedTo("user"), checkOutSession)
-=======
->>>>>>> 4d095729625eebbe768adb66c39e6a9dcd350368
 
 
 export default Router
